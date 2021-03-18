fix mixed precision

utils/cuda.py
@@ -39,14 +39,15 @@ class NativeScaler:
     def __init__(self):
         self._scaler = torch.cuda.amp.GradScaler()
 
-    def __call__(self, loss, optimizer, clip_grad=None, parameters=None, create_graph=False):
+    def __call__(self, loss, optimizer, create_graph=False):
         self._scaler.scale(loss).backward(create_graph=create_graph)
+        
+    
+    def step(self, optimizer, clip_grad=None, parameters=None):
         if clip_grad is not None:
             assert parameters is not None
             self._scaler.unscale_(optimizer)  # unscale the gradients of optimizer's assigned params in-place
             torch.nn.utils.clip_grad_norm_(parameters, clip_grad)
-    
-    def step(self,optimizer):
         self._scaler.step(optimizer)
         self._scaler.update()
 