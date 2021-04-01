445a178387ae9254fb4ab8ca60d4e9a74dec9a74
kaylode <kaylode@github.com>
2021-04-01 11:49:44 +0700

    fix visualization

--- utils/utils.py
+++ utils/utils.py
@@ -4,12 +4,12 @@ from PIL import Image
 def draw_retrieval_results(queries, top_k_relevant, save_filename):
     ax = []
     post_list = queries + top_k_relevant
-    fig=plt.figure(figsize=(30, 20))
+    fig=plt.figure(figsize=(10, 10))
     columns = len(top_k_relevant)
     rows = len(queries)
     for i in range(columns*rows):
-        img = Image.open(post_list[i-1][0])
+        img = Image.open(post_list[i][0])
         ax.append(fig.add_subplot(rows, columns, i+1) )
-        ax[-1].set_title(post_list[i-1][1])  # set title
+        ax[-1].set_title(post_list[i][1])  # set title
         plt.imshow(img)
     plt.savefig(save_filename)
