4d7ed688a8edbd6f485fe458f66dce9a41c45c84
Kaylode <kayp.kieran@gmail.com>
2021-09-18 18:36:10 +0700

    change loss

--- train.py
+++ train.py
@@ -92,7 +92,7 @@ def train(args, config):
     print(f'Training with {num_gpus} gpu(s): ')
     print(devices_info)
     print(f"Start training at [{start_epoch}|{start_iter}]")
-    print(f"Current best BLEU: {best_value}")
+    print(f"Current best MAP: {best_value}")
 
     trainer.fit(start_epoch = start_epoch, start_iter = start_iter, num_epochs=config.num_epochs, print_per_iter=args.print_per_iter)
 
