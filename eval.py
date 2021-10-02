1778aeecb86156780b049a371a73a833b3620fab
Kaylode <kayp.kieran@gmail.com>
2021-10-02 22:34:29 +0700

    add evaluation

---
+++ eval.py
@@ -0,0 +1,53 @@
+from utils.getter import *
+import argparse
+
+parser = argparse.ArgumentParser('Training Object Detection')
+parser.add_argument('--top_k', type=int, default=10, help='Retrieve top k results')
+parser.add_argument('--weight', type=str, default=None,
+                    help='whether to load weights from a checkpoint, set None to initialize')
+
+torch.backends.cudnn.benchmark = True
+torch.backends.cudnn.fastest = True
+seed_everything()
+
+def train(args, config):
+    os.environ['CUDA_VISIBLE_DEVICES'] = config.gpu_devices
+    num_gpus = len(config.gpu_devices.split(','))
+
+    device = torch.device("cuda" if torch.cuda.is_available() else 'cpu')
+    devices_info = get_devices_info(config.gpu_devices)
+    
+    _, valset, _, _ = get_dataset_and_dataloader(config)
+
+    net = TERN(1024)
+
+    metric = RetrievalScore(
+            valset, valset, 
+            max_distance = 1.3,
+            top_k=args.top_k,
+            save_results=True)
+
+    model = Captioning(model = net, device=device)
+  
+    if args.weight is not None:                
+        load_checkpoint(model, args.weight)
+        
+    print()
+    print("##########   DATASET INFO   ##########")
+    print("Valset: ")
+    print(valset)
+    print()
+    print(config)
+    print(f'Evaluating with {num_gpus} gpu(s): ')
+    print(devices_info)
+
+    metric.update(model)
+    metric_results = metric.value()
+    print(metric_results)
+
+if __name__ == '__main__':
+    
+    args = parser.parse_args()
+    config = Config(os.path.join('configs','config.yaml'))
+
+    train(args, config)
