b8572c818c7d8e19a38b341c18ba0478e8344c56
Kaylode <kayp.kieran@gmail.com>
2021-10-05 20:21:04 +0700

    add base config

--- configs/config.py
+++ configs/config.py
@@ -39,7 +39,7 @@ def config_from_dict(_dict, ignore_keys=[]):
     """
     Load config from dict
     """
-    config = Config('./configs/config.yaml')
+    config = Config('./configs/yaml/base.yaml')
     for k,v in _dict.items():
         if k not in ignore_keys:
             config.__setattr__(k,v)
