1bb73a3465f2b50190f7ca471a4cf2ce7cc7ec5c
Kaylode <kayp.kieran@gmail.com>
2021-10-10 23:38:22 +0700

    add importing sgraf

--- models/encoder/__init__.py
+++ models/encoder/__init__.py
@@ -1,4 +1,4 @@
 from .language import EncoderBERT
 from .visual import EncoderBottomUp, EncoderVIT
 from .projection import *
-from .modal import ViTR, TERN
+from .modal import ViTR, TERN, SGRAF
--- models/encoder/modal/__init__.py
+++ models/encoder/modal/__init__.py
@@ -1,3 +1,4 @@
 from models.encoder.modal.vitr import ViTR
 from .tern import TERN
 from .vitr import ViTR
+from .sgraf import SGRAF
